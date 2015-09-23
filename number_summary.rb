9f19c500fe00544e23c7523f3975423cd9738a18
unknown <rodriguezmelojp@MVHS-CS15-20.dcsdk12.org>
2015-09-23 11:19:20 -0600

    finish

--- number_summary.rb
+++ number_summary.rb
@@ -1,10 +1,75 @@
-# @author Tim Padjen <tpadjen@gmail.com>
+# @author Michelle Mao  <mm321whatsup@gmail.com>
 class NumberSummary
-	
 	class << self
+		
+		def convert_to_float(file)
+			file = File.open("data.csv", 'r')
+			line = file.each_line.first
+			array_1 = line.split(',')
+			array = []
+			for n in 0...array_1.length
+				array.push(array_1[n.to_f])
+			end
+		end
 
-		# code goes here
+			def max(array)
+				return array.max
+			end
 
-	end
+			def min(array)
+				return array.min  
+			end
 
+			def mean(array)
+				y = 0.0
+				for x in 0...array.length	
+					y += array[x]
+				end
+				y / array.length 
+			end
+			
+			# def mode(array)
+			# mode = Hash.new(0)
+			# 	array.each do |n|
+			# 		if mode.has_key?(n)
+			# 			mode[n] = mode[n] + 1
+			# 		else
+			# 			mode[n] = 1
+			# 		end
+			# 	end
+			# return mode.select { |k,v| v == mode.values.max}.keys
+			# end
+			def mode(array)
+				array = array.sort
+				array_key = []
+				array_value = []
+				for n in 0...array.length
+					if array_key.include? n
+
+					else
+						array_key.push(array[n])
+						array_value.push()
+
+				end
+			end
+
+			def median(array)
+				if array.length % 2 == 0
+					array = array.sort
+					return (array[array.length/2] + array[(array.length/2) - 1])/ 2.0
+				else
+					array = array.sort
+					return array[(array.length - 1)/ 2]
+				end	
+			end
+
+ 
+
+
+
+
+
+
+
+ end
 end
---
+++ test.rb
@@ -0,0 +1,55 @@
+require 'minitest/autorun'
+
+
+require_relative 'number_summary'
+
+class NumberSummaryTest < MiniTest::Test
+	
+end
+
+module NumberSummaryTests
+	class Max < NumberSummaryTest
+		def test_max
+			assert_equal 10, NumberSummary.max([0, 1, 2, 3, 4, 5,6,7, 8,9 ,10 ])
+			assert_equal 8, NumberSummary.max([8, 8, 8, 8, 8, 8,8,8,8])
+			assert_equal -1, NumberSummary.max([-1, -2, -3 ,-4, -5, -6 ,-7 ,-8 ,-10])
+			assert_equal 9.89, NumberSummary.max([1.3,4.8,7.6,5.5,-8.9,9.89])
+		end
+	end
+	class Min < NumberSummaryTest
+		def test_min
+			assert_equal 0, NumberSummary.min([0, 1, 2, 3, 4, 5,6,7, 8,9 ,10 ])
+			assert_equal 8, NumberSummary.min([8, 8, 8, 8, 8, 8,8,8,8])
+			assert_equal -10, NumberSummary.min([-1, -2, -3 ,-4, -5, -6 ,-7 ,-8 ,-10])
+		end
+	end
+	class Mean < NumberSummaryTest
+		def test_mean
+			assert_equal 5, NumberSummary.mean([0,10])
+			assert_equal 25, NumberSummary.mean([0, 0, 0, 100])
+			assert_equal 7.5, NumberSummary.mean([0,15])
+			assert_equal 5.75, NumberSummary.mean([0,11.5])
+		end
+	end
+	class Mode < NumberSummaryTest
+		def test_mode
+			assert_equal [1], NumberSummary.mode([1,1,1,1,1,2,2,6,7,8,6,7,8,])
+			assert_equal [1,2,3,45,67,], NumberSummary.mode([1,2,3,45,67])
+			assert_equal [4.8], NumberSummary.mode([4.8,4.8,6.0,5.0,2.3,2.3,4.8])	
+		end
+	end
+	class Median < NumberSummaryTest
+		def test_median
+			assert_equal 3 , NumberSummary.median([1,2,3,4,5])
+			assert_equal 4.5 , NumberSummary.median([3,6,7,8,2,1,5,4])
+		end
+	end
+
+
+
+
+end
+
+
+
+
